dbaf598364990867fef0cea51b4f35c6c922bcb1
ABD <abishekeditingdoit@gmail.com>
2026-08-05 20:58:26 +0530

    Time: 184 ms (61.72%), Space: 307.3 MB (64.14%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& graph, vector<bool>& suspicious) {
+        suspicious[node] = true;
+
+        for (int i = 0; i < graph[node].size(); i++) {
+            int next = graph[node][i];
+            if (!suspicious[next]) {
+                dfs(next, graph, suspicious);
+            }
+        }
+    }
+
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+
+        vector<vector<int>> graph(n);
+
+        for (int i = 0; i < invocations.size(); i++) {
+            int u = invocations[i][0];
+            int v = invocations[i][1];
+            graph[u].push_back(v);
+        }
+
+        vector<bool> suspicious(n, false);
+
+        dfs(k, graph, suspicious);
+
+        for (int i = 0; i < invocations.size(); i++) {
+            int u = invocations[i][0];
+            int v = invocations[i][1];
+
+            if (!suspicious[u] && suspicious[v]) {
+                vector<int> ans;
+                for (int j = 0; j < n; j++) {
+                    ans.push_back(j);
+                }
+                return ans;
+            }
+        }
+
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
